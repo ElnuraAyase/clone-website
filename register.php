45dabaf7c24df5c672edc0ed0c69e84de549eb15
Elnura <elya.ayase@gmail.com>
2024-02-03 02:54:47 +0400

    Create register.php

---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+include 'config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST[''], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, email, password_hash) VALUES ('$username', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
